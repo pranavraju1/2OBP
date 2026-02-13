fc6f009b1161ff8ceda91f0a3cf0785e62c44b11
pranavraju1 <pranavraju2020@gmail.com>
2026-02-13 06:04:52 +0530

    12-02-26 push

---
+++ 12-02-26/client/src/pages/gigs/Gigs.jsx
@@ -0,0 +1,92 @@
+import React, { useRef, useState } from "react";
+import "./Gigs.css";
+// import { gigs } from "../../data";
+import GigCard from "../../components/gigCard/GigCard";
+import { useQuery } from "@tanstack/react-query";
+import newRequest from "../../utils/newRequest";
+import { useLocation } from "react-router-dom";
+
+function Gigs() {
+  
+  //storing the value of sort selected 
+  // currently we are storing as sales as this would be applied in the API req from backend
+  const [sort, setSort] = useState("sales");
+
+  // menu open and close
+  const [open, setOpen] = useState(false);
+  
+  const minRef = useRef();
+  const maxRef = useRef();
+
+  const {search} = useLocation();
+
+  const {isLoading, error, data, refetch} = useQuery({
+    queryKey: ['gigs'],
+    // queryFn: () => newRequest('/gigs').then((res)=>{
+    queryFn: () => newRequest.get(`/gigs/${search}&min=${minRef.current.value}&max=${maxRef.current.value}&sort=${sort}`)
+    .then((res)=>{
+      return res.data;
+    })
+  })
+
+  console.log(data);
+
+
+
+
+
+  const reSort = (type) => {
+    setSort(type);
+    setOpen(false);
+  };
+
+  const apply = ()=>{
+    refetch();
+  }
+
+  return (
+    <div className="gigs">
+      <div className="container">
+        <span className="breadcrumbs">Hirely > Graphics & Design ></span>
+        <h1>AI Artists</h1>
+        <p>
+          Explore the boundaries of art and technology with Hirely's AI artists
+        </p>
+        <div className="menu">
+          <div className="left">
+            <span>Budget</span>
+            <input ref={minRef} type="number" placeholder="min" />
+            <input ref={maxRef} type="number" placeholder="max" />
+            <button onClick={apply}>Apply</button>
+          </div>
+          <div className="right">
+            <span className="sortBy">Sort by</span>
+            <span className="sortType">
+              {sort === "sales" ? "Best Selling" : "Newest"}
+            </span>
+            <img src="./img/down.png" alt="" onClick={() => setOpen(!open)} />
+            {open && (
+              <div className="rightMenu">
+                {sort === "sales" ? (
+                  <span onClick={() => reSort("createdAt")}>Newest</span>
+                ) : (
+                  <span onClick={() => reSort("sales")}>Best Selling</span>
+                  )}
+                  <span onClick={() => reSort("sales")}>Popular</span>
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="cards">
+            {
+              isLoading ? "loading" : error ? "Something went wrong" :
+              (data.map((gig)=><GigCard key={gig._id} item={gig} />))
+            }
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Gigs;
---
+++ 12-02-26/server/server.js
@@ -0,0 +1,43 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import cookieParser from 'cookie-parser'
+import connectDB from './config/mongoDb.js'
+
+import userRoutes from './routes/userRoutes.js'
+import authRoutes from './routes/authRoutes.js'
+import gigRoutes from './routes/gigRoutes.js'
+
+
+const app = express();
+
+const PORT = process.env.PORT || 4000
+
+app.use(express.json());
+
+// this is done so that we can send cookies in requests from the express app
+app.use(cookieParser());
+
+// ycors is for us to connect with the client side
+// you have to set credentials: true to pass cookies from client to server
+app.use(cors({origin:'http://localhost:5173', credentials: true}))
+
+app.use((err, req, res, next)=>{
+    const errorStatus = err.status || 500    
+    const errorMessage = err.message || 'Something went wrong'
+    return res.status(errorStatus).send(errorMessage)
+})
+
+app.get('/',(req,res)=>{
+    res.send('api is working')
+});
+
+app.use('/api/user',userRoutes);
+app.use('/api/auth',authRoutes);
+app.use('/api/gigs',gigRoutes);
+
+
+app.listen(PORT, ()=>{
+    console.log(`server is running on port: ${PORT}`)
+})
+connectDB();
